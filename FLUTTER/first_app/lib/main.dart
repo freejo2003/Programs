9c04a8e5c75240613c2397206b8e1d7496cde11d
Freejo Jaison <freejojaison555@gmail.com>
2024-02-24 20:10:25 +0530

    Day 2

--- FLUTTER/first_app/lib/main.dart
+++ FLUTTER/first_app/lib/main.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
-void main(){ 
-  runApp();   
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    ),
+  );
 }
