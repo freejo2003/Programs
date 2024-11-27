804db09b06ed5f039a1eaab519add0714b56334b
unknown <freejojaison555@gmail.com>
2024-11-28 04:17:08 +0530

    day 2

---
+++ manifold/string_1.py
@@ -0,0 +1,40 @@
+# In a database there are 'n' students. The fields of the table are name, age, grade, and gender separated by comma. Your task is to print the student's names who are greater than or equal to 20 years old and print the average grade of female candidates using ASCII values of female candidate's grades.
+
+# Input Format
+
+# Get the value of 'n' as the first line of input.
+# Get 'n' student's details, as comma separated input and each input in a newline.
+
+# Constraints
+
+# NA
+
+# Output Format
+
+# Print the student's names separated by a space in first line of output.
+# If all student's ages are less than 20 years old, you are supposed to print "ALL INELIGIBLE".
+# Print the average grade of female students in the second line of output.
+# If there are no female students in the database, you are supposed to print "NO FEMALE STUDENTS"
+
+# Sample Input 0
+
+# 3
+# AAA,19,B,FEMALE
+# BBB,22,A,MALE
+# CCC,24,C,FEMALE
+# Sample Output 0
+
+# BBB CCC
+# 66.50
+# Sample Input 1
+
+# 5
+# XYZ,19,C,FEMALE
+# FFF,19,A,FEMALE
+# FGD,18,B,FEMALE
+# DGS,17,B,FEMALE
+# ESD,17,C,FEMALE
+# Sample Output 1
+
+# ALL INELIGIBLE
+# 66.20
